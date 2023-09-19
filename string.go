a07cea882e618496f8bac993108f0564ceba516f
Sanika Mg <sanikamg9@gmail.com>
2023-09-19 15:52:45 +0530

    sting conversion added

--- string.go
+++ string.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	//append string
@@ -9,9 +12,18 @@ func main() {
 	str1 += str2
 	fmt.Println(str1)
 	//slice of string
-	fmt.Println(str1[0:])  //sanika mg
-	fmt.Println(str1[1:])  //anika mg
-	fmt.Println(str1[:3])  //san
-	fmt.Println(str1[1:3]) //an
-	fmt.Println(str1[:])   //sanika mg
+	fmt.Println(str1[0:])    //sanika mg
+	fmt.Println(str1[1:])    //anika mg
+	fmt.Println(str1[:3])    //san
+	fmt.Println(str1[1:3])   //an
+	fmt.Println(str1[:])     //sanika mg
+	num := strconv.Itoa(123) //int to string
+	fmt.Println(num)
+	str, _ := strconv.Atoi("123")
+	fmt.Println(str)
 }
+
+//String
+
+//-string is immutable
+//-we can't change string after created
