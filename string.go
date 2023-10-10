84cc10edb58adb533dcc08790c3be5bd0b6e7659
Sanika Mg <sanikamg9@gmail.com>
2023-10-10 19:47:04 +0530

    linked list started

--- string.go
+++ string.go
@@ -12,11 +12,12 @@ func main() {
 	str1 += str2
 	fmt.Println(str1)
 	//slice of string
-	fmt.Println(str1[0:])    //sanika mg
-	fmt.Println(str1[1:])    //anika mg
-	fmt.Println(str1[:3])    //san
-	fmt.Println(str1[1:3])   //an
-	fmt.Println(str1[:])     //sanika mg
+	fmt.Println(str1[0:])  //sanika mg
+	fmt.Println(str1[1:])  //anika mg
+	fmt.Println(str1[:3])  //san
+	fmt.Println(str1[1:3]) //an
+	fmt.Println(str1[:])   //sanika mg
+
 	num := strconv.Itoa(123) //int to string
 	fmt.Println(num)
 	str, _ := strconv.Atoi("123")
