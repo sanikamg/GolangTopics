b6fc8659294fb93531442345d697d87767b46568
Sanika Mg <sanikamg9@gmail.com>
2023-10-24 23:30:00 +0530

    ascending completed

--- array.go
+++ array.go
@@ -16,7 +16,7 @@ func main() {
 
 	// Define a custom sorting function
 	sort.Slice(arr, func(i, j int) bool {
-		return arr[i][1] < arr[j][1]
+		return arr[i][1] > arr[j][1]
 	})
 
 	// Print the sorted array
